330711aca48fe339ea633aae0d9ca79524ff50ac
Brenda <49522696+bitwoman@users.noreply.github.com>
2021-05-11 16:44:38 -0300

    part03

--- part03.sql
+++ part03.sql
@@ -125,7 +125,7 @@
 		@nQtdMediaConsumida = AVG(nQuantidade)
 	FROM tMOVPedido AS Pedido 
 	JOIN tMOVPedidoItem AS Item ON (Pedido.iIDPedido = Item.iIDPedido)
-	WHERE Item.IDLivro = 8513 AND Pedido.dPedido >= DATEADD(MONTH,-6,@dReferencia /*getdate()*/ )
+	WHERE Item.IDLivro = 8513 AND Pedido.dPedido >= DATEADD(MONTH,-6,@dReferencia /*GETDATE()*/ )
 
 /* 
  * Calcula a quantidade que de ser solicitada
@@ -150,6 +150,181 @@
 
 
 	SELECT * FROM tMOVSolicitacaoCompra
+
+/* BEGIN / END
+
+	Agrupa várias instruções em um bloco de comandos. 
+
+	BEGIN
+	   <Comandos>
+	   <Comandos>
+		...
+	END
+
+	Melhor organização de código.
+ */
+
+	BEGIN
+	   /*
+	   Iniciando a consulta
+	   */
+	   SET NOCOUNT ON
+
+	   SELECT 
+			cTitulo 
+	   FROM tCADLivro 
+	   WHERE iIDLivro = 3443
+
+	   RAISERROR('Consulta realizada com sucesso', 10, 1)
+
+	END 
+	GO
+
+/*
+ * Blocos Aninhados 
+ */
+	BEGIN
+	   SET NOCOUNT ON
+/*
+ * Iniciando a consulta
+ */
+	   BEGIN 
+      
+		  SELECT cTitulo 
+  		  FROM tCADLivro 
+		  WHERE iIDLivro = 3443
+
+		  RAISERROR('Consulta realizada com sucesso', 10, 1)
+
+	   END 
+
+/*
+ * Confirma o pedido 
+ */
+	   BEGIN 
+
+		  UPDATE tMOVPedido 
+		  SET dCancelado = GETDATE() 
+		  WHERE iidpedido = 182965
+
+		  RAISERROR('Pedido cancelado', 10,1) 
+
+	   END 
+	END 
+	GO
+
+/*
+ * Se tirar todos os blocos, o script será igual ao exemplo abaixo 
+ */
+
+/*
+ * Iniciando a consulta
+ */
+	SET NOCOUNT ON
+	
+	SELECT cTitulo 
+	FROM tCADLivro 
+	WHERE iIDLivro = 3443
+	
+	RAISERROR('Consulta realizada com sucesso',10,1)
+
+/*
+ * Confirma o pedido 
+ */
+	UPDATE tMOVPedido 
+	SET dCancelado = GETDATE() 
+	WHERE iidpedido = 182965
+	
+	RAISERROR('Pedido cancelado', 10,1) 
+
+/*
+ * Exemplo de script com comentários, definição de variáveis e bloco BEGIN/END
+ */
+	BEGIN 
+
+	   SET NOCOUNT ON
+
+/* Procedimento para calcular o consumo médio de um livro nos último 6 meses, 
+ * calcular a previsão de consumo para os próximos 12 meses e gerar uma 
+ * solicitação de compras de livro. 
+ */
+		
+		DECLARE @iidSolicitacao int        
+		DECLARE @iIDLivro int				   
+		DECLARE @nPeso numeric(13,1)		   
+		DECLARE @nQtdMesesConsumo int		  
+		DECLARE @nQtdEstoque int			   
+		DECLARE @nQtdMediaConsumida int		
+		DECLARE @nQtdSolicitada int			
+		DECLARE @mValorEstimado smallmoney  
+		DECLARE @mPesoEstimado numeric(13,1) 
+		DECLARE @dReferenciaData DATE
+		DECLARE @dReferencia DATETIME
+
+/* Necessário criar uma variável como DATE para setar a data, e posteriormente, 
+ * convertê-la para DATETIME. Do contrário, dará erro.
+ */
+		SET @dReferenciaData = '2018-09-15'
+
+		SET @iIDLivro = 8513 
+		SET @dReferencia = @dReferenciaData
+	
+/* Recupera o Peso atual do livro e a 
+ * quantidade de meses prevista para consumo.
+ */
+		SELECT 
+				@nPeso = nPeso, 
+				@nQtdMesesConsumo = nMesesConsumo  
+		FROM tCADLivro 
+		WHERE iIDLivro = @iIDLivro
+
+/* Calcula o estoque atual do livro e o valor médio para estimativa da compra. 
+ */
+		SELECT 
+				@nQtdEstoque =  SUM(nQuantidade),
+				@mValorEstimado = AVG(mValor) 
+		FROM tRELEstoque 
+		WHERE iIDLivro = @iIDLivro
+
+/* Calcula a quantidade média consumida nos últimos seis meses. 
+ */
+		SELECT 
+			@nQtdMediaConsumida = AVG(nQuantidade)
+		FROM tMOVPedido AS Pedido 
+		JOIN tMOVPedidoItem AS Item ON (Pedido.iIDPedido = Item.iIDPedido)
+		WHERE Item.IDLivro = @iIDLivro
+		AND dPedido <= DATEADD(month,-6, @dReferencia  /*GETDATE()*/  )
+
+/*
+ * Calcula a quantidade que deve ser solicitada
+ */
+		SET @nQtdSolicitada = (@nQtdMediaConsumida * @nQtdMesesConsumo ) - @nQtdEstoque
+
+/*
+ * Calcula o valor estimado da quantidade solicitada
+ */ 
+		SET @mValorEstimado = @mValorEstimado * @nQtdSolicitada
+
+/*
+ * Calcula o peso estimado
+ */ 
+		SET @mPesoEstimado = @nQtdSolicitada * @nPeso
+
+/*
+ * Inclui a solicitação de compras
+ */ 
+		SET @iidSolicitacao = NEXT VALUE FOR seqiIDSolicitacao
+
+		INSERT INTO tMOVSolicitacaoCompra (iidSolicitacao,iIDLivro, nQuantidade , mValorEstimado, mPesoEstimado)
+		VALUES (@iidSolicitacao,@iIDLivro,@nQtdSolicitada , @mValorEstimado, @mPesoEstimado)
+
+	END 
+/*
+ * Fim do cálculo de consumo médio 
+ */
+	SELECT * FROM tMOVSolicitacaoCompra
+
+
 
 
 
